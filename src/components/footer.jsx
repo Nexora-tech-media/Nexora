403dfa0775220c65cbed306662b526b2510b6cda
nexora-tech-media <coomedia.info@gmail.com>
2026-04-19 19:18:10 +0300

    Removed footer quick links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,13 +9,6 @@ const Footer = () => {
     <div className='px-5 bg-white dark:bg-black text-black dark:text-white'>
         <h1 className='text-center header-sm md:header-md lg:header-lg'>Footer</h1>
         <div className="">
-            <h2 className='paragraph-sm md:paragraph-md lg:paragraph-lg py-5 font-medium'>Quick Links:</h2>
-            <div className="w-full list-none flex gap-5 paragraph-sm md:paragraph-md lg:paragraph-lg">
-              <li className='font-secondary hover:text-primary-500 dark:hover:text-Secondary-500'><a href="/">Home</a></li>
-              <li className='font-secondary hover:text-primary-500 dark:hover:text-Secondary-500'><a href="/">Services</a></li>
-              <li className='font-secondary hover:text-primary-500 dark:hover:text-Secondary-500'><a href="/">Products</a></li>
-              <li className='font-secondary hover:text-primary-500 dark:hover:text-Secondary-500'><a href="/">About</a></li>
-            </div>
             <div>
               <h2 className='paragraph-sm md:paragraph-md lg:paragraph-lg py-5 font-medium'>Our social media accounts:</h2>
               <div className='list-none flex lg:flex-row gap-5 flex-col paragraph-sm md:paragraph-md lg:paragraph-lg'>
